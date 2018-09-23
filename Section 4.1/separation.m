e8d3a4ebc01ad3d7b89a9b7466656caf3d557494
marieshifeng <marie.feng@gmail.com>
2018-09-23 14:14:59 -0400

    added more graph photos

--- Section 4.1/separation.m
+++ Section 4.1/separation.m
@@ -5,11 +5,15 @@ load points.mat
 
 % measured_distances is an array of distances in pixels between red spot and
 % blue spot
-load measured_distances.mat
+load distances.mat
 
 % calibration_distance is the distance in pixels of the 11" long-side
 % of the white sheet in DSC_0242.JPG
 load calibration_distance.mat
+
+% gold is the experimental data we measured between the light and the
+% pinholes wall, which we use to compare with our calculated predictions
+load gold.mat
 
 % calibration_constant relates inches to pixels
 calibration_constant = 11 / calibration_distance;
@@ -23,7 +27,7 @@ x = 16;
 p = 1;
 
 % d is an array of calibrated measured distances
-d = measured_distances * calibration_constant;
+d = distances * calibration_constant;
 
 % Z is an array of calculated distances between the camera and light
 Z = [];
@@ -35,8 +39,20 @@ end
 figure
 scatter(d, Z, 'filled')
 hold on;
-err = 8*ones(size(Z));
-errorbar(d, Z, err, 'LineStyle','none');
+scatter(d, gold, 'filled')
+legend('Prediction data (d)','Experiemental data (gold)');
+temp = gold - Z;
+yneg = zeros(size(temp));
+ypos = zeros(size(temp));
+for i = 1:size(temp,2)
+    if (temp(i) < 0)
+        yneg(i) = -temp(i);
+    else
+        ypos(i) = temp(i);
+    end
+end
+% errorbar(d, Z, yneg, ypos, 'LineStyle','none');
 xlabel('Measured distance between 2 spots in inches (d)')
-ylabel('Calculated distance between camera and light in inches (Z)') 
+ylabel('Calculated distance between camera and light in inches (Z)')
+ylim([2.5 55])
 title('Relationship between Z and d')
